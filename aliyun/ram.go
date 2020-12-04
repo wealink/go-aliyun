a991a09672a5e50f57911e024b7046bebe879b78
hongbozhao <995977154@qq.com>
2020-12-04 11:47:39 +0800

    aliyun

---
+++ aliyun/ram.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ram"
+	"log"
+	"os"
+	"strconv"
+)
+
+var client *ram.Client
+var err error
+
+func Init() {
+	var (
+		region string
+		key    string
+		secret string
+	)
+	flag.StringVar(&region, "r", "cn-shanghai", "region")
+	flag.StringVar(&key, "k", "", "accessKeyId")
+	flag.StringVar(&secret, "s", "", "accessSecret")
+	flag.Parse()
+	client, err = ram.NewClientWithAccessKey(region, key, secret)
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+}
+
+func GetUserList() {
+	request := ram.CreateListUsersRequest()
+	request.Scheme = "https"
+
+	response, err := client.ListUsers(request)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	policies := make(map[string]string)
+	//fmt.Printf("response is %#v\n", response.Users.User)
+	for _, v := range response.Users.User {
+		//fmt.Println(GetUserPolicy(v.UserName))
+		policies[v.UserName] = GetUserPolicy(v.UserName)
+
+	}
+	//for k,v := range policies{
+	//	fmt.Println(k,v)
+	//}
+	WriteExcel(policies)
+}
+
+func GetUserPolicy(username string) string {
+	request := ram.CreateListPoliciesForUserRequest()
+	request.Scheme = "https"
+	request.UserName = username
+	response, err := client.ListPoliciesForUser(request)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	//fmt.Printf("response is %#v\n", response.Policies)
+	policies := ""
+	for _, v := range response.Policies.Policy {
+		//policies = append(policies, v.PolicyName)
+		//policies = append(policies, v.PolicyName)
+		policies += v.PolicyName + ","
+	}
+	return policies
+}
+
+func WriteExcel(policies map[string]string) {
+	xlsx := excelize.NewFile()
+	index := xlsx.NewSheet("userpolicy")
+	i := 1
+	for k, v := range policies {
+		//设置单元格的值
+		fmt.Println("A"+strconv.Itoa(i), k)
+		fmt.Println("B"+strconv.Itoa(i), v)
+		xlsx.SetCellValue("userpolicy", "A"+strconv.Itoa(i), k)
+		xlsx.SetCellValue("userpolicy", "B"+strconv.Itoa(i), v)
+		i++
+	}
+	xlsx.SetActiveSheet(index)
+	err := xlsx.SaveAs("./policy.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	Init()
+	GetUserList()
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module go-example
+
+go 1.15
+
+require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1 // indirect
+	github.com/360EntSecGroup-Skylar/excelize/v2 v2.3.1 // indirect
+	github.com/aliyun/alibaba-cloud-sdk-go v1.61.738 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/xuri/efp v0.0.0-20201016154823-031c29024257 // indirect
+	golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c // indirect
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
+	golang.org/x/text v0.3.4 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+)
